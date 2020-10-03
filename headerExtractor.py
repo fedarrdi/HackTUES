a15f90a9e2e9bbecd729d597c8e324a47effe1ec
fedarrdi <42972410+fedarrdi@users.noreply.github.com>
2020-10-03 12:59:39 +0300

    Update headerExtractor.py

--- headerExtractor.py
+++ headerExtractor.py
@@ -1,3 +1,3 @@
 def headerExtractor(soup):
     for heading in soup.find_all(re.compile(r'^h[1-6]$|^p$')):  #re.compile tursi pattern ^nachalo &kray
-        print(heading.name + ' ' + heading.text.strip()+"\n")
+        print(heading.text.strip()+"\n")
